a8bd750d146ad432e73165d01c7570fb40544743
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-02 20:21:01 -0300

    feat: add tailwind merge to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,21 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ type = "button", ...props }: ButtonProps) {
-  return <button type={type} {...props} />;
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ type = "button", className, ...props }) => {
+    return (
+      <button
+        type={type}
+        {...props}
+        className={twMerge(
+          "w-full h-10 rounded-xl bg-[#85A98F] hover:opacity-85",
+          className
+        )}
+      />
+    );
+  }
+);
+
+Button.displayName = "Button";
