e4636f60d60e8197bd369edb353aa08bb7d9ee4d
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-02 15:04:30 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+
+const rubik = Rubik({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Z",
+  description:
+    "Este é uma aplicação desenvolvida no curso de ReactJS da B7Web.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={rubik.className}>{children}</body>
+    </html>
+  );
+}
