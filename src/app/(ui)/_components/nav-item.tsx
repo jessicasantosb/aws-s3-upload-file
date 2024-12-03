678be15665dec0805e7fde3c12263a95907216ff
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-03 19:02:17 -0300

    feat: create navItem component

---
+++ src/app/(ui)/_components/nav-item.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { IconDefinition } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavItemProps = {
+  label: string;
+  icon: IconDefinition;
+  href: string;
+  active?: boolean;
+};
+
+export function NavItem({ label, icon, href, active }: NavItemProps) {
+  const pathname = usePathname();
+
+  const isPath = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center gap-6  ${
+        active || isPath ? "opacity-100" : "opacity-60"
+      } hover:opacity-100`}
+    >
+      <FontAwesomeIcon icon={icon} className="size-6" />
+      <p className="text-lg">{label}</p>
+    </Link>
+  );
+}
