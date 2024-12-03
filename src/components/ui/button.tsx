9c8b261f36068cdeebc4f4f1e958b6edd2b803d7
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-03 08:27:08 -0300

    feat: create button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,18 +1,40 @@
+import { cva, VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+const buttonVariants = cva(
+  "w-full flex items-center justify-center whitespace-nowrap rounded-xl hover:opacity-80",
+  {
+    variants: {
+      variant: {
+        default: "bg-[#85A98F]",
+        outline: "border border-2 bg-white text-black hover:bg-black/10",
+        error: "bg-red-500",
+        success: "bg-green-500",
+      },
+      size: {
+        default: "h-10 px-4",
+        sm: "h-9 px-3",
+        lg: "h-11 px-8",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ type = "button", className, ...props }) => {
+  ({ type = "button", variant, size, className, ...props }) => {
     return (
       <button
         type={type}
         {...props}
-        className={twMerge(
-          "w-full h-10 rounded-xl bg-[#85A98F] hover:opacity-85",
-          className
-        )}
+        className={twMerge(buttonVariants({ variant, size, className }))}
       />
     );
   }
