ae663dddaf3735ecb9dcc4478abef7163cf13a10
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-03 11:05:24 -0300

    style: add placeholder

--- src/app/(auth)/_components/signin-form.tsx
+++ src/app/(auth)/_components/signin-form.tsx
@@ -4,8 +4,8 @@ import { Input } from "@/components/ui/input";
 export function SigninForm() {
   return (
     <form className="mt-10 flex flex-col gap-4">
-      <Input type="email" />
-      <Input type="password" />
+      <Input type="email" placeholder="Digite seu email" />
+      <Input type="password" placeholder="Digite sua senha" />
       <Button>entrar</Button>
     </form>
   );
