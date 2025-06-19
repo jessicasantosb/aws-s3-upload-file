77bb578bc2eb3e28aee634af39473721c8e5bf86
Jessica <jessica.santosb@outlook.com>
2025-06-19 11:57:59 -0300

    refactor: add eslint file

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,7 +1,6 @@
-import { ReactNode } from "react";
-
 import { LeftSidebar } from "@/app/(ui)/_components/_left-sidebar";
 import { RightSidebar } from "@/app/(ui)/_components/_right-sidebar";
+import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import { Logo } from "@/components/logo";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect('/home')
-
-  return (
-    <main className="h-screen flex items-center justify-center">
-      <Logo size={80} />
-    </main>
-  );
+  redirect("/home");
 }
