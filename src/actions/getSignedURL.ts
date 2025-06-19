a438f4a90c81e5ec3f448e79c099334bf2a52143
Jessica <jessica.santosb@outlook.com>
2025-06-19 17:24:36 -0300

    feat: configure aws s3 client connection

---
+++ src/actions/getSignedURL.ts
@@ -0,0 +1,28 @@
+'use server'
+
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3 = new S3Client({
+  region: process.env.AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+export const getSignedURL = async () => {
+  const session = true;
+  if (!session) return { failure: "Não autenticado" };
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: "test-file",
+  });
+
+  const signedUrl = await getSignedUrl(s3, putObjectCommand, {
+    expiresIn: 60,
+  });
+
+  return { success: { url: signedUrl } };
+};
